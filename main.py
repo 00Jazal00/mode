4fae578091fa11a58c3353e84d14a5c6976cb9da
nomanjazal <nomanjazal@gmail.com>
2024-03-27 19:19:46 -0700

    Update

---
+++ main.py
@@ -0,0 +1,22 @@
+import json
+import os
+
+json_file_path = "airplane_mode.json"
+def get_airplane_mode_status():
+    with open(json_file_path, "r") as file:
+        data = json.load(file)
+        return data["airplane_mode"]
+
+def set_airplane_mode_status(status):
+    with open(json_file_path, "r+") as file:
+        data = json.load(file)
+        data["airplane_mode"] = "0"
+        file.seek(0)
+        json.dump(data, file, indent=4)
+        file.truncate()
+
+
+current_status = get_airplane_mode_status()
+if current_status == "1":
+    set_airplane_mode_status("0")
+    os.system('git add . && git commit -m "Update" && git push')
